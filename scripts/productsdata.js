b610b67f487ec106c05321bb2875929f073aa5c4
MUHAMMED FAHIZ P A <fahisayub@gmail.com>
2022-03-29 16:33:48 +0530

    first commit

---
+++ scripts/productsdata.js
@@ -0,0 +1,216 @@
+//data of Blue heaven cosmetics products
+var bheavenProducts=[
+    
+    {
+        image_url:'https://3.imimg.com/data3/WJ/EO/MY-3792183/01-500x500.jpg' ,
+        productName :'Sindoor' ,
+        category:'Traditional Items',
+        price:100 ,
+        brand:'Blue Heaven Cosmetics' ,
+        location:'Kirti Nagar, New Delhi, Delhi' ,
+    }
+    ,
+    {
+        image_url:'https://3.imimg.com/data3/QC/FL/MY-3792183/fairness-scrub-500x500.jpg' ,
+        productName :'Fairness Scrub' ,
+        category:'Makeup',
+        price:199 ,
+        brand:'Blue Heaven Cosmetics' ,
+        location:'Kirti Nagar, New Delhi, Delhi' ,
+    }
+    ,
+    {
+        image_url:'https://3.imimg.com/data3/NL/QG/MY-3792183/fruit-bleach-500x500.jpg' ,
+        productName :'Fruit Bleach' ,
+        category:'Bleach Cream',
+        price:259 ,
+        brand:'Blue Heaven Cosmetics' ,
+        location:'Kirti Nagar, New Delhi, Delhi' ,
+    },
+    {
+        image_url:'https://3.imimg.com/data3/US/UJ/MY-3792183/lip-balm-500x500.jpg' ,
+        productName :'Lip Balm' ,
+        category:'LipCare',
+        price:58 ,
+        brand:'Blue Heaven Cosmetics' ,
+        location:'Kirti Nagar, New Delhi, Delhi' ,
+    }
+    ,{
+        image_url:'https://3.imimg.com/data3/EJ/OU/MY-3792183/lip-gaurd-500x500.jpg' ,
+        productName :'Lip Gaurd' ,
+        category:'LipCare',
+        price:88 ,
+        brand:'Blue Heaven Cosmetics' ,
+        location:'Kirti Nagar, New Delhi, Delhi' ,
+    }
+    ,{
+        image_url:'https://4.imimg.com/data4/QI/AY/GLADMIN-3792183/2-250x250.jpg' ,
+        productName :'Coral Red Lipstick' ,
+        category:'LipCare',
+        price:60 ,
+        brand:'Blue Heaven Cosmetics' ,
+        location:'Kirti Nagar, New Delhi, Delhi' ,
+    }
+    ,{
+        image_url:'https://4.imimg.com/data4/KD/IX/GLADMIN-3792183/4-250x250.jpg' ,
+        productName :'Pretty Purple Lipstick' ,
+        category:'LipCare',
+        price:40 ,
+        brand:'Blue Heaven Cosmetics' ,
+        location:'Kirti Nagar, New Delhi, Delhi' ,
+    }
+    ,{
+        image_url:'https://3.imimg.com/data3/ML/RO/MY-3792183/mascara-500x500.jpg' ,
+        productName :'Mascara' ,
+        category:'Eyes Products',
+        price:115 ,
+        brand:'Blue Heaven Cosmetics' ,
+        location:'Kirti Nagar, New Delhi, Delhi' ,
+    }
+    ,{
+        image_url:'https://3.imimg.com/data3/FX/IN/MY-3792183/eye-shadow-500x500.jpg' ,
+        productName :'Eye Shadow' ,
+        category:'Eyes Products',
+        price:237 ,
+        brand:'Blue Heaven Cosmetics' ,
+        location:'Kirti Nagar, New Delhi, Delhi' ,
+    }
+    ,{
+        image_url:'https://3.imimg.com/data3/GE/KN/MY-3792183/gold-bleach-500x500.jpg' ,
+        productName :'Gold Bleach' ,
+        category:'Bleach Cream',
+        price:338 ,
+        brand:'Blue Heaven Cosmetics' ,
+        location:'Kirti Nagar, New Delhi, Delhi' ,
+    }
+    ,{
+        image_url:'https://3.imimg.com/data3/UP/UB/MY-3792183/rose-water-mask-250x250.jpg' ,
+        productName :'Rose Water -Mask' ,
+        category:'Rose Water',
+        price:220 ,
+        brand:'Blue Heaven Cosmetics' ,
+        location:'Kirti Nagar, New Delhi, Delhi' ,
+    }
+    ,{
+        image_url:'https://3.imimg.com/data3/BK/MD/MY-3792183/01-250x250.jpg' ,
+        productName :'Nail Enamel' ,
+        category:'Nails Products',
+        price:405 ,
+        brand:'Blue Heaven Cosmetics' ,
+        location:'Kirti Nagar, New Delhi, Delhi' ,
+    }
+    ,{
+        image_url:'https://4.imimg.com/data4/AP/MT/GLADMIN-3792183/1-250x250.jpg' ,
+        productName :'Diamond Facial Kit' ,
+        category:'Facial Kit',
+        price:250 ,
+        brand:'Blue Heaven Cosmetics' ,
+        location:'Kirti Nagar, New Delhi, Delhi' ,
+    }
+    ,{
+        image_url:'https://3.imimg.com/data3/BH/LB/MY-3792183/massage-cream-250x250.jpg' ,
+        productName :'Massage Cream' ,
+        categary:'Creams',
+        price:125 ,
+        brand:'Blue Heaven Cosmetics' ,
+        location:'Kirti Nagar, New Delhi, Delhi' ,
+    }
+    ,];
+
+    //data of ready to ship Products
+
+   var readytoShip= [
+        {
+            image_url:"https://3.imimg.com/data3/BX/CS/MY-3792183/foundation-250x250.jpg" ,
+            productName :'Foundation' ,
+            price:150 ,
+            brand:'Mansi Beauty Zone' ,
+            location:'Madurai, Tamil Nadu' ,
+        },
+        {
+            image_url:'https://5.imimg.com/data5/ANDROID/Default/2021/2/AE/QR/RK/123514038/img-20210220-wa0143-jpg-250x250.jpg' ,
+            productName :'Mac Studio Foundation' ,
+            price:390 ,
+            brand:'Dream Shades' ,
+            location:'Pattanakad, Alappuzha, Kerala' ,
+        }
+        ,
+        {
+        image_url:'https://5.imimg.com/data5/ECOM/Default/2022/3/GC/CG/RT/59895549/1-2-500x500.jpg' ,
+        productName :'Beaded Beauty -multicolor' ,
+        price: 1139,
+        brand:'Indus Trade Links Private Limited' ,
+        location:'Kirti Nagar, New Delhi, Delhi' ,
+    }
+    ,{
+        image_url:'https://5.imimg.com/data5/ECOM/Default/2022/2/ZP/EF/XV/50366780/img-20210910-wa0213-500x500.jpg' ,
+        productName :'Aglonima Lipstick' ,
+        price: 100,
+        brand:'EWSELLS CHARITY STORE' ,
+        location:'Kirti Nagar, New Delhi, Delhi' ,
+    }
+    ,{
+        image_url:'https://5.imimg.com/data5/ECOM/Default/2021/11/QO/YM/CD/81412824/c1157078-1-500x500.jpg' ,
+        productName :'Color Glam London Lipsticks' ,
+        price: 299,
+        brand:'Worldshopon' ,
+        location:'Kirti Nagar, New Delhi, Delhi' ,
+    }
+    ,{
+        image_url:'https://5.imimg.com/data5/ECOM/Default/2022/3/OR/CK/RV/139286493/ezgif-5-2bc60a5c81-500x500.jpg' ,
+        productName :'QIBEST Matte Lipstick-Velvet' ,
+        price: 500,
+        brand:'Aadhyacollections' ,
+        location:'Kirti Nagar, New Delhi, Delhi' ,
+    }
+    ,{
+        image_url:'https://5.imimg.com/data5/ECOM/Default/2022/3/AY/DC/WE/129740345/product-image-1879821350-500x500.jpg' ,
+        productName :'QIBEST Liquid Lipstick' ,
+        price: 599,
+        brand:'Beautify Collections India' ,
+        location:'Kirti Nagar, New Delhi, Delhi' ,
+    }
+    ,{
+        image_url:'https://5.imimg.com/data5/ECOM/Default/2022/2/VI/VR/DK/147540438/product-image-1733246360-500x500.jpg' ,
+        productName :'Hot Matte Lipstick P-Shape' ,
+        price: 288,
+        brand:'Garima Collection' ,
+        location:'Kirti Nagar, New Delhi, Delhi' ,
+    },
+];
+ 
+// data of related products
+
+var relatedcategories=[
+    {
+        image_url:'https://3.imimg.com/data3/EP/VJ/GLADMIN-105850/cosmetics-250x250.jpg' ,
+        category :'Cosmetics' ,
+        
+    },
+    {
+        image_url:'https://5.imimg.com/data5/JM/OT/KQ/GLADMIN-68087/cosmetic-third-party-manufacturing-250x250.jpg' ,
+        category :'Cosmetic Third Party Manufacturing' ,
+        
+    },
+    {
+        image_url:'https://3.imimg.com/data3/TT/GW/GLADMIN-21908/beauty-cosmetics-250x250.jpg' ,
+        category :'Beauty Cosmetics' ,
+        
+    },
+    {
+        image_url:'https://5.imimg.com/data5/ZB/JY/DJ/GLADMIN-68087/matte-lipstick-250x250.png' ,
+        category :'Matte Lipstick' ,
+        
+    },
+    {
+        image_url:'https://3.imimg.com/data3/KG/HH/GLADMIN-11601/lip-balm-250x250.jpg' ,
+        category :'Lip Balm' ,
+        
+    },
+    {
+        image_url:'https://3.imimg.com/data3/IF/CY/GLADMIN-108453/liquid-lipstick-250x250.jpg' ,
+        category :'Liquid Lipstick' ,
+        
+    },
+
+]
